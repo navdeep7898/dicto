e8607d3654612d3b6da0d3f9348d28a9cfa25d2d
Hne <hemantgupta0404@gmail.com>
2021-04-08 10:10:10 +0530

    modified templates

--- dictionary/dicto/views.py
+++ dictionary/dicto/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render , redirect
+from django.contrib import messages
 from dicto.models import Words
 
 # Create your views here.
@@ -7,36 +8,48 @@ def base(request):
 
 def index(request):
     if request.method =="POST":
-        w = request.POST.get("word")
-        word = Words.objects.get(eword=w)
-        if str(w) == str(word):
+        w = request.POST.get("word").lower()
+        l = Words.objects.filter(eword=w).exists()
+        if l:
+            word = Words.objects.get(eword=w)
             return render(request,'index.html',{"word":word})
+        else:
+            messages.success(request,"Word is not Present in Dictionary ! ")
+            return redirect('/')
 
 
 
 def edit(request , eword):
     word = Words.objects.get(eword=eword)
     if request.method == "POST":
-        hword = request.POST.get("hword")
-        uses = request.POST.get("use")
-        rword = request.POST.get("rword")
+        hword = request.POST.get("hword").lower()
+        uses = request.POST.get("use").lower()
+        rword = request.POST.get("rword").lower()
         word.hword = hword
         word.uses = uses
         word.rword = rword
         word.save()
+        messages.success(request , "Word Edit Successfully !")
         return redirect("/" )
     return render(request, 'edit.html' , {"word":word})
     
 def add(request):
     if request.method == "POST":
-        w = Words()
-        w.eword = request.POST.get("word")
-        w.hword = request.POST.get("hword")
-        w.uses = request.POST.get("use")
-        w.rword = request.POST.get("rword")
-        w.save()
-        msg = "Word Added Succesfully"
-        return render(request,'add.html',{"msg":msg})
+        eword = request.POST.get("word").lower()
+        l = Words.objects.filter(eword=eword).exists()
+        if l:
+            messages.success(request ,"Word already in Dictionary !")
+            return redirect("/")
+        
+        else:
+            w = Words()
+            w.eword = request.POST.get("word").lower()
+            w.hword = request.POST.get("hword").lower()
+            w.uses = request.POST.get("use").lower()
+            w.rword = request.POST.get("rword").lower()
+            w.save()
+            messages.success(request,"Word Added Succesfully ! ")
+            return render(request,'add.html')
     return render(request, 'add.html')
 
 
@@ -47,4 +60,5 @@ def show(request):
 def delete(request,eword):
     word = Words.objects.get(eword=eword)
     word.delete()
+    messages.success(request,"Word Delete Successfully ! ")
     return redirect("/")
